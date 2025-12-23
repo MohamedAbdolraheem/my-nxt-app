197a9af5b1032499af15b8f7437094960bb960de
Mohammed <mohamed.abdolraheem.55@gmail.com>
2025-12-23 19:39:19 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
 
 export default nextConfig;
